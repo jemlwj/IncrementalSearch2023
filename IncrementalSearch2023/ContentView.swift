2cdfe9ef8c587ea90cf170a7fa1faf5d21c550c7
Jeremy Lua <jemlwjdev@gmail.com>
2023-02-17 10:24:08 +0900

    Initial Commit

---
+++ IncrementalSearch2023/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IncrementalSearch2023
+//
+//  Created by Jeremy Lua on 17/2/23.
+//  Copyright © 2023 Jeremy Lua. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IncrementalSearch2023Tests/IncrementalSearch2023Tests.swift
@@ -0,0 +1,34 @@
+//
+//  IncrementalSearch2023Tests.swift
+//  IncrementalSearch2023Tests
+//
+//  Created by Jeremy Lua on 17/2/23.
+//  Copyright © 2023 Jeremy Lua. All rights reserved.
+//
+
+import XCTest
+@testable import IncrementalSearch2023
+
+class IncrementalSearch2023Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
