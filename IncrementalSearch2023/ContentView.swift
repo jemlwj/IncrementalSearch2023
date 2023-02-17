4ad8d42773f3651b7fd32d075c5fa1559e6d2dbc
Jeremy Lua <jemlwjdev@gmail.com>
2023-02-17 18:23:52 +0900

    Add query textfield

--- IncrementalSearch2023/ContentView.swift
+++ IncrementalSearch2023/ContentView.swift
@@ -9,12 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var query: String = ""
+
     var body: some View {
-        
-        List(array, id: \.identifier) { item in
-            VStack(alignment: .leading, spacing: 5) {
-                Text(item.full_name)
-                Text(item.html_url)
+        VStack {
+            //Searchable is possible but due to development environment limitations, TextField is used instead.
+            TextField(
+                "Enter search query",
+                text: $query,
+                onCommit: {
+                    //When return is pressed on the keyboard, run this block
+                    //run API here
+                }
+            )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+            
+            //List of Item objects to be populated here
+            List(array, id: \.identifier) { item in
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(item.full_name)
+                    Text(item.html_url)
+                }
             }
         }
     }
